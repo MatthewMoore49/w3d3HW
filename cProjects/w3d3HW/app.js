2f4463c2fdedf883538cc472d74b4431e88bac6e
Matt <msm3390@gmail.com>
2021-04-14 22:21:25 -0400

    1st

---
+++ cProjects/w3d3HW/app.js
@@ -0,0 +1,80 @@
+
+//1. Declare ten different variables and assign them the value of a number.
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+//2. Two times, console.log the addition of two of the variables you declared.
+function addTwo(h,j){
+answer = h + j
+console.log(answer);
+}
+function addTwo(b,j){
+    answer = b + j
+    console.log(answer);
+    }
+//3. Two times, console.log the subtraction of two of the variables you declared.
+function subtractTwo(i,c){
+     answer = i - c
+     console.log(answer);
+}
+function subtractTwo(h,a){
+    answer = h - a
+    console.log(answer);
+}
+//4. Two times, console.log the multiplication of two of the variables you declared.
+function multiplyTwo(d,e){
+    answer = d * e
+    console.log(answer);
+}
+function multiplyTwo(f,h){
+    answer = f * h
+    console.log(answer);
+}
+//5. Two times, console.log the division of two of the variables you declared.
+function divideTwo(h,b){
+    answer = h / b
+    console.log(answer);
+}
+function divideTwo(j,e){
+    answer = j / e
+    console.log(answer);
+}
+//6. Two times, increments one of the variables you declared.
+
+var h = 8;
+h++;
+
+var j = 10;
+j++;
+//7. Two times, decrement one of the variables you have declared.
+var i = 9;
+i--;
+
+var g = 7
+g--;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
